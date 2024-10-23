62f9fb9e01e38b0d4d66ca7e60991ccd74cc399d
chailinyu <cly0016@qq.com>
2024-10-23 10:34:45 +0800

    first commit

---
+++ 未命名1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#ibclude <stdlib.h>
+int main(){
+	float x, y, a;
+	for(y=1.5;y>-1.5;y-=0.1)
+	{
+		a=x*x+y*y-1;
+		putchar(a*a*a-x*x*y*y*y<=0.0?'*':'')
+			}
+			system("color 0c");
+			putchar('\n');
+			printf("崔梓辰！要哦开心\n");
+			printf("天天开心\n");
+			return 0;
+}
